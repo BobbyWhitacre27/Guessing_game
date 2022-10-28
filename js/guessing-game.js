9299d6985768cd684b90a3c1c9ea00c935d48b0e
BobbyWhitacre27 <bobby.whitacre27@gmail.com>
2022-10-28 18:44:48 -0400

    JS working for 1 guess attempt

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -20,20 +20,10 @@ a user clicks a button or adds a guess to the input field.
 
 */
 
-//
 
-// 1. create a random number between 1-100
-
-function randomNumberGenerator(min, max){
-        let randomValue = Math.floor(Math.random() * (100 - 1+ 1) + 1)
-        return randomValue
-      }
-console.log(randomNumberGenerator(1,100))
-
-// 2. if the input is higher than random value, answer needs to say lower. if the input is lower than randomvalue, answer needs to say higher.
-// 3. if the is equal to the random value then answer needs to say you win!
-
-function guess(inputValue) {
+function guessingGame(inputValue) {
+    let randomValue = Math.floor(Math.random() * (100 - 1 + 1) + 1)
+    //console.log(randomValue)
     if (inputValue === randomValue){
         return `YOU WIN!!, the random number is ${randomValue}` 
     }else if (inputValue > randomValue){
@@ -42,6 +32,45 @@ function guess(inputValue) {
         return "Guess higher"
     }
 }
+
+var submitGuessButton = document.getElementById("guessButton")
+var inputValue = document.getElementById("inputValue").value
+var outputMessage = document.getElementById("outputMessage")
+var playAgainButton = document.getElementById("playAgainButton")
+var hintButton = document.getElementById("hintButton")
+var randomNumber = document.getElementById("randomNumber")
+
+var g1 = document.getElementById("g1")
+var g2 = document.getElementById("g2")
+var g3 = document.getElementById("g3")
+var g4 = document.getElementById("g4")
+var g5 = document.getElementById("g5")
+
+
+function changeg1(){
+    var inputValue = document.getElementById("inputValue").value;
+    g1.textContent = `Guess #1: ${inputValue}`;
+}
+
+submitGuessButton.addEventListener("click",function changeg1(){
+    var inputValue = document.getElementById("inputValue").value;
+    g1.textContent = `Guess #1: ${inputValue}`;
+})
+
+submitGuessButton.addEventListener("click",function guessingGame() {
+    var inputValue = document.getElementById("inputValue").value
+    let randomValue = Math.floor(Math.random() * (100 - 1 + 1) + 1)
+    randomNumber.textContent = `(Random Number is ${randomValue})`
+    //console.log(randomValue)
+    if (inputValue === randomValue){
+        return outputMessage.textContent = `YOU WIN!!, the random number is ${randomValue}` 
+    }else if (inputValue > randomValue){
+        return outputMessage.textContent = "Guess lower"
+    }else if (inputValue < randomValue){
+        return outputMessage.textContent = "Guess higher"
+    }
+})
+
 
 // 4. after 5 guesses then the correct answer appears and the game is over
 
